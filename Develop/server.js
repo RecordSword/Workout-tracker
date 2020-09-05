fc2f84b31557e9e8fbcae3acb40e883c29d85c23
Nick  Sword <nicksword@hotmail.com>
2020-09-05 20:58:32 +0800

    Working project

--- Develop/server.js
+++ Develop/server.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
-const PORT = process.env.PORT || 3000
+const morgan = require("morgan");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(morgan("tiny"))
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
-// routes
-app.use(require("./routes/api.js"));
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.listen(PORT, function () {
+    console.log("==> Listening on port %s. Visit http://localhost:%s in your browser", PORT, PORT)
 });
